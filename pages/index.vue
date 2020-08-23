f25ca03b1d18898af83984d844d463061514aa12
Krista <blueirisbooks@gmail.com>
2020-08-23 17:19:17 -0500

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <ul>
+          <li><nuxt-link to="/blog/blog-post">Blog Post 1</nuxt-link></li>
+          <li><nuxt-link to="/blog/blog-post">Blog Post 2</nuxt-link></li>
+          <li><nuxt-link to="/blog/blog-post">Blog Post 3</nuxt-link></li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
